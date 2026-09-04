7a64c35ff5109fcbc90dc7a0684d988b3294b986
Ishant Kumavat <ishantkumavat79@gmail.com>
2026-09-04 22:47:56 +0530

    Delete all occurrences of a key in DLL

--- Doubly LL/02_Delete_All_Occurrences_Of_A_Key_In_DLL.cpp
+++ Doubly LL/02_Delete_All_Occurrences_Of_A_Key_In_DLL.cpp
@@ -1,8 +1,12 @@
 // GFG => Delete all occurrences of a key in DLL
 
+// Optimal Solution =>
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
-// a Node of the doubly linked list
+// Structure of the doubly linked list  Node 
 class Node {
   public:
     int data;
@@ -10,31 +14,44 @@ class Node {
     Node* prev;
 
     Node(int x) {
-    data = x;
-    next = nullptr;
-    prev = nullptr;
-  }
+        data = x;
+        next = nullptr;
+        prev = nullptr;
+    }
 };
-
 class Solution {
   public:
     Node* deleteAllOccurOfX(Node* head, int x) {
-        if(head == nullptr || (head -> next == nullptr && head -> data == x)) return nullptr;
         Node* temp = head;
         while(temp != nullptr){
             if(temp -> data == x){
-                if(temp == head){
-                    head = head -> next;
-                }
-                Node* last = temp -> prev; 
-                Node* front = temp -> next; 
-                if(last) last -> next = front;
-                if(front) front -> prev = last;
-                delete temp;
-                temp = front;
+              Node* front = temp -> next;
+              if(temp == head) head = front;
+              else{
+                Node* prev = temp -> prev;
+                prev -> next = front;
+                if(front) front -> prev = prev;
+              }
+              temp = front;
             }
             else temp = temp -> next;
         }
         return head;
     }
 };
+
+// Traverse the doubly linked list.
+//
+// If the current node contains x,
+// remove it from the list.
+//
+// If the node is the head,
+// update head to the next node.
+//
+// Otherwise, connect the previous node
+// with the next node.
+//
+// Update the prev pointer of the next node
+// if it exists.
+//
+// Move temp to the next node after deletion.
